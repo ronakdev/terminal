cdb783b95cb43f2188f2d8e5002a63947adc7024
Ronak Shah <ronakshahultimate@gmail.com>
2016-08-31 23:04:41 -0700

    Started on basic tab completition

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var wage = document.getElementById("navigation");
+wage.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+        changePage(e);
+    }
+    else if (e.keyCode == 9) {
+        tabComplete(e);
+    }
+});
+
+function changePage(e) {
+    var destination = document.getElementById("navigation").value;
+   console.log(destination);
+}
+
+function tabComplete(e) {
+    var destination = document.getElementById("navigation");
+
+    console.log("Tab: " + destination.value);
+    if (destination.value.substring(0,1) == "p" ) {
+        destination.value = "projects";
+    }
+    else if (destination.value.substring(0,1))
+    e.preventDefault();
+}
